c325a6bee5c5bbe3281421ba2fdd760d09af8875
Pavel Tsakalidis <p@vel.gr>
2025-11-08 19:35:40 +0000

    Convert choices.js init to a function

--- src/commands/report/templates/scripts.js
+++ src/commands/report/templates/scripts.js
@@ -74,5 +74,30 @@ const ChoicesTemplateManager = {
                 ${html}
                 ${label}
                 </div>`
+    },
+
+    init_with_count: function (element, placeholder) {
+        return new Choices(
+                element,
+                {
+                    removeItemButton: true,
+                    searchResultLimit: -1,
+                    placeholderValue: placeholder,
+                    allowHTML: true,
+                    shouldSort: false,
+                    callbackOnCreateTemplates: function (strToEl, escapeForTemplate) {
+                        let classNames = this.config.classNames;
+                        let itemSelectText = this.config.itemSelectText;
+                        return {
+                            item: function({ classNames }, data) {
+                                return ChoicesTemplateManager.item(strToEl, escapeForTemplate, classNames, data, '');
+                            },
+                            choice: function ({ classNames }, data) {
+                                return ChoicesTemplateManager.choice(strToEl, escapeForTemplate, classNames, itemSelectText, data, `<span class="badge text-bg-primary me-2" style="width: 40px;">${$(data.element).data('count')}</span>`);
+                            }
+                        }
+                    }
+                }
+            );
     }
 };
